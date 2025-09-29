972f4f0afa72f5adbcd0bbaba3217bc57de25e0c
Rohan kumar rout <rohanrout1122@gmail.com>
2025-09-29 05:40:57 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";  //treat all js code as newer version
+
+// alert(3+3)  //we are using nodejs,not browser  
+
+//console.log(3+3);//code readability shpould be high
+
+let name="rohan"
+let age = 18
+let isLoggedIn=false
+
+//number=> 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefiend=>
+//symbol=> unique
+
+
+//object
+console.log(typeof('hitesh'))
