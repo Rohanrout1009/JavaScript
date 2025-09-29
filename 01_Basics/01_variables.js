40b861ef83b244cce41de7a242e791ce685b67b6
Rohan kumar rout <rohanrout1122@gmail.com>
2025-09-29 05:40:36 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=12345
+let accountEmail="rohan@gmail.com"
+let accountCity="jaipur"
+
+accountCity="Bangaluru"
+/*
+Prefer not to use var
+beacuse of issue in block scope and functional scope
+
+*/
+   
+console.log(accountCity);
+console.table([accountId,accountCity,accountEmail])
+
